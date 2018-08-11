7f69fdb8c2f6120034de6d56f75597aaeb9204d1
Phan Nhat Minh <pnminh1710@gmail.com>
2018-08-11 19:29:50 +0700

    improve code

--- src/Input/FileUpload/index.jsx
+++ src/Input/FileUpload/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './styles.css';
 
+const ERROR_MESSAGE = 'Some of your file(s) does not match, please re-upload';
+
+
 class ImageUpload extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       files: [],
+      error: null,
+      isUploaded: false,
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -14,34 +19,47 @@ class ImageUpload extends React.Component {
 
   handleChange(event) {
     const { files } = event.target;
+    let error = null;
     const numOfFile = files.length;
+    const allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
     let previewFiles = [];
-    for (let i = 0 ; i < numOfFile; i++) {
+    for (let i = 0; i < numOfFile; i++) {
       previewFiles[i] = {
         file: files[i],
         name: files[i].name,
         path: URL.createObjectURL(files[i]),
       };
     };
-    const imgList = previewFiles.map((file, index) =>
-    <div className="container">
-      <img key={index} alt={file.name} src={file.path} />
-    </div>
-    );
+    const imgList = previewFiles.map((file, index) => {
+      if (!allowedExtensions.exec(file.name)) {
+        error = ERROR_MESSAGE;
+        return null;
+      }
+      return (
+        <div key={index} className="container">
+          <img alt={file.name} src={file.path} />
+        </div>
+      );
+    });
     this.setState({
       files: imgList,
+      isUploaded: true,
+      error,
     });
   }
 
   render() {
-    const { files } = this.state;
+    const { files, isUploaded, error } = this.state;
     return (
       <div>
-        <input id="img-upload-input" type="file" onChange={this.handleChange} multiple/>
-        <label className="upload-label" htmlFor="img-upload-input" >Choose a file</label>
+        <input
+          id="img-upload-input" type="file" onChange={this.handleChange}
+          multiple data-filetype="jpg|jpeg|png" />
+        {!isUploaded && <label className="upload-label" htmlFor="img-upload-input" >+</label>}
         <div className="wrapper">
           {files}
         </div>
+        {error && <p className="error">{error}</p>}
       </div>
     )
   }
